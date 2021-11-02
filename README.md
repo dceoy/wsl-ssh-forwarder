bdf8860c5c601b3ce2cdef78f8e59a2c3c75146f
dceoy <dnarsil+github@gmail.com>
2021-11-03 05:46:52 +0900

    Remove forward_ssh_port.sh

--- README.md
+++ README.md
@@ -3,19 +3,12 @@ wsl-ssh-forwarder
 
 SSH Forwarder for WSL2
 
-1.  Install Git and SSH in WSL.
+1.  Install SSH in WSL.
 
     ```sh
-    $ sudo apt -y install git ssh
+    $ sudo apt -y install ssh
     ```
 
 2.  Edit /etc/ssh/sshd_config in WSL.
 
-3.  Put `forward_ssh_port.sh` in WSL.
-
-    ```sh
-    $ git clone https://github.com/dceoy/wsl-ssh-forwarder.git
-    $ sudo cp wsl-ssh-forwarder/forward_ssh_port.sh /usr/local/bin/
-    ```
-
-4.  Set `forward_ssh_port.bat` on Task Scheduler in Windows.
+3.  Set `forward_ssh_port.bat` on Task Scheduler in Windows.
--- forward_ssh_port.bat
+++ forward_ssh_port.bat
@@ -1,2 +1,6 @@
-wsl --exec sudo sudo service ssh restart
-wsl --exec bash /usr/local/bin/forward_ssh_port.sh
+wsl --user root --exec service ssh restart
+wsl --user root --exec netsh.exe interface portproxy delete v4tov4 listenport=22
+$ip = wsl --exec bash -c "ip address show eth0 | sed -ne 's/^ *inet \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\/.*$/\1/p'"
+wsl --user root --exec netsh.exe interface portproxy add v4tov4 listenport=22 connectport=22 listenaddress=localhost connectaddress=$ip
+wsl --user root --exec sc.exe config iphlpsvc start=auto
+wsl --user root --exec sc.exe start iphlpsvc
